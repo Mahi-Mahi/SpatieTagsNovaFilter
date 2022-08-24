8ae64814a5a323038347b10cf93e07bc62fe2fed
olivM <olivM@users.noreply.github.com>
2022-08-24 15:18:03 +0000

    Fix styling

--- src/AutocompleteController.php
+++ src/AutocompleteController.php
@@ -19,8 +19,9 @@ class AutocompleteController
         $tagModel = config('tags.tag_model', Tag::class); //https://github.com/spatie/laravel-tags/blob/cd2ee039cac108761e1e92558046a6428f052199/src/HasTags.php#L18
 
         $query = $tagModel::query();
-        if ($request->get('q'))
+        if ($request->get('q')) {
             $query = $query->containing($request->get('q'));
+        }
         if ($request->get('tag_type')) {
             $query = $query->where('type', $request->get('tag_type'));
         }
